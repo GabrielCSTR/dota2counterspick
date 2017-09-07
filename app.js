f2c77d04c5f9a3cfe550a2c0cdb554b4c6079d05
Gabriel Carlos <Gabriel Carlos>
2017-09-07 19:41:33 -0300

    update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var express = require('express');
+var app     = express();
 const SteamUser = require('steam-user');
 const client = new SteamUser();
 //var sleep = require('sleep');
@@ -110,3 +112,14 @@ client.on('friendsList', function () {
 });
 */
 
+
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
