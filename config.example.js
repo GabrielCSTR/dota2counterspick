8d7d1fd7b62f431e1ed4cd905864be7b825cbd3c
XxStR <gabrielcarlos91@hotmail.com>
2017-09-26 20:21:17 -0300

    Configuration bot steam

---
+++ config.example.js
@@ -0,0 +1,9 @@
+// Configure and rename to "config.js"
+var config = {};
+
+config.steam_name = "NAMEBOT";
+config.steam_user = "USERNAME_BOT";
+config.steam_pass = "PASSWORD_BOT";
+//config.steam_guard_code = "";
+
+module.exports = config;
